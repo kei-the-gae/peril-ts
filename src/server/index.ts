eedc7904dd3b35affea58ba79fe573631288ca67
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2026-01-27 09:00:11 -0800

    connect to rabbitmq

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,25 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+    const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+    const conn = await amqp.connect(rabbitConnString);
+    console.log("Peril game server connected to RabbitMQ!");
+
+    ["SIGINT", "SIGTERM"].forEach((signal) =>
+        process.on(signal, async () => {
+            try {
+                await conn.close();
+                console.log("RabbitMQ connection closed.");
+            } catch (err) {
+                console.error("Error closing RabbitMQ connection:", err);
+            } finally {
+                process.exit(0);
+            }
+        }),
+    );
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
+    console.error("Fatal error:", err);
+    process.exit(1);
 });
