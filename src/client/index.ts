b54c4fa31e03e5cdfcf0a2b9495c84d3972ce680
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2026-01-28 20:14:03 -0800

    add move handler

--- src/client/index.ts
+++ src/client/index.ts
@@ -11,11 +11,17 @@ import {
     SimpleQueueType,
     subscribeJSON,
 } from "../internal/pubsub/consume.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import {
+    ArmyMovesPrefix,
+    ExchangePerilDirect,
+    ExchangePerilTopic,
+    PauseKey,
+} from "../internal/routing/routing.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
 import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { commandMove } from "../internal/gamelogic/move.js";
-import { handlerPause } from "./handlers.js";
+import { handlerMove, handlerPause } from "./handlers.js";
+import { publishJSON } from "../internal/pubsub/publish.js";
 
 async function main() {
     const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -37,6 +43,16 @@ async function main() {
 
     const username = await clientWelcome();
     const gs = new GameState(username);
+    const publishCh = await conn.createChannel();
+
+    await subscribeJSON(
+        conn,
+        ExchangePerilTopic,
+        `${ArmyMovesPrefix}.${username}`,
+        `${ArmyMovesPrefix}.*`,
+        SimpleQueueType.Transient,
+        handlerMove(gs),
+    );
 
     await subscribeJSON(
         conn,
@@ -55,7 +71,13 @@ async function main() {
         const command = words[0];
         if (command === "move") {
             try {
-                commandMove(gs, words);
+                const move = commandMove(gs, words);
+                publishJSON(
+                    publishCh,
+                    ExchangePerilTopic,
+                    `${ArmyMovesPrefix}.${username}`,
+                    move,
+                );
             } catch (err) {
                 console.log((err as Error).message);
             }
